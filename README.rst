fd33b1e2b9a704ae7242b6c33f52aa9e9e315683
chillaranand <anand21nanda@gmail.com>
2017-03-20 17:33:37 +0530

    Updated package metadata

---
+++ README.rst
@@ -0,0 +1,11 @@
+axmlparser
+===========
+
+
+A simple parser to parse Android XML file.
+
+
+Usage
+======
+
+See examples folder on how to use this package.
--- setup.py
+++ setup.py
@@ -3,15 +3,29 @@ from setuptools import find_packages, setup
 
 dependencies = open('requirements.txt').read().splitlines()
 
+with open('README.rst') as fh:
+    long_description = fh.read()
+
+
 setup(
     name='pyaxmlparser',
     version='0.1.3',
     url='https://github.com/appknox/pyaxmlparser',
+
+    author='Appknox',
+    author_email='engineering@appknox.com',
+
+    description='A simple parser to parse Android XML file',
+    long_description=long_description,
+
     packages=find_packages(exclude=['tests']),
     zip_safe=False,
     platforms='any',
+
     install_requires=dependencies,
     py_modules=['pyaxmlparser'],
+
+    keywords='appknox axmlparser arscparser android',
     classifiers=[
         'Development Status :: 4 - Beta',
 
