1ebe9942f7dd8fa018b4d354f6a4808243e7563d
chillaranand <anand21nanda@gmail.com>
2017-03-20 12:10:30 +0530

    Removed unused import

--- pyaxmlparser/axmlparser.py
+++ pyaxmlparser/axmlparser.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from struct import pack, unpack
+from struct import unpack
 from warnings import warn
 
 import pyaxmlparser.constants as const
