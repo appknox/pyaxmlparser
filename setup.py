b7acba1491f65f39701e5c2875a39d8879236f25
chillaranand <anand21nanda@gmail.com>
2017-07-14 14:49:51 +0530

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pyaxmlparser',
-    version='0.3.3',
+    version='0.3.4',
     url='https://github.com/appknox/pyaxmlparser',
 
     author='Subho Halder',
