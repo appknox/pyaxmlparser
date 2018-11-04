ed83eaf5e177707d72ebc2f45530d06599fe2923
Subho Halder <subho.halder@gmail.com>
2018-11-04 22:30:27 +0530

    Bump version: 0.3.11 → 0.3.12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 __VERSION__ = '0.3.11'
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(
