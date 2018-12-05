794011abf86710979930c9a14e8941b70dc83210
Yury Shumilov <y.shumilov@drweb.com>
2018-12-05 14:50:31 +0300

    - fix invalid view readme on github - added python_requires for view on https://pypi.org .

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import pyaxmlparser
 here = path.abspath(path.dirname(__file__))
 
 
-with open(path.join(here, 'README.rst'), 'r', encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -22,6 +22,7 @@ setup(
     author_email=pyaxmlparser.__author_email__,
     license=pyaxmlparser.__license__,
     packages=find_packages(exclude=['tests', 'examples']),
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     include_package_data=True,
     zip_safe=False,
     platforms='any',
