1d57cbf0cc72a3d9f9d0efcc1d41c83a0fb77f7b
Subho Halder <subho.halder@gmail.com>
2021-03-09 03:13:23 +0530

    Added signing check

--- pyaxmlparser/cli.py
+++ pyaxmlparser/cli.py
@@ -16,3 +16,7 @@ def main(filename):
     click.echo('Package: {}'.format(apk.packagename))
     click.echo('Version name: {}'.format(apk.version_name))
     click.echo('Version code: {}'.format(apk.version_code))
+    click.echo('Is it Signed: {}'.format(apk.signed))
+    click.echo('Is it Signed with v1 Signatures: {}'.format(apk.signed_v1))
+    click.echo('Is it Signed with v2 Signatures: {}'.format(apk.signed_v2))
+    click.echo('Is it Signed with v3 Signatures: {}'.format(apk.signed_v3))
